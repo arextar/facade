0e2218176adcb000127189604b8720c96ad04953
arexkun <arexkun11@gmail.com>
2011-11-12 12:55:16 -0500

    add facade.replace helper function

--- facade.js
+++ facade.js
@@ -7,6 +7,30 @@ function Facade(a){
     this.doc = a.length ? a : [a];
 }
 
+function render( wi, what, parent ){
+    wi = parse( wi&&wi.call?
+        wi(what)
+    :
+        wi
+    )
+    if(parent){
+        var x = 0, e;
+        for(;e=wi[x++];e.parent=parent);
+    }
+    return wi;
+}
+
+function facade(html){
+    return new Facade(
+        ""+html === html? parse(html) : html
+    )
+}
+
+var replace = facade.replace = function( what, wi ){
+    var parent = what.parent;
+    [].splice.apply(v.parent, [v.parent.indexOf(what), 1].concat( render(wi, what, parent) ));
+}
+
 Facade.prototype = {
     render: function(min){
         return build(min? minify(this.doc) : this.doc);
@@ -28,10 +52,7 @@ Facade.prototype = {
         var rep = find( this.doc, sel ), x = 0, v, fn = typeof w == "function";
         
         for(; v = rep[x++];){
-            [].splice.apply(v.parent,
-                [v.parent.indexOf(v),
-                1].concat( parse( fn? w(v) : w ) )
-            );
+            replace(v, w);
         }
         
         return this;
@@ -114,11 +135,5 @@ Facade.prototype = {
         return this;
     }
 }
-
-function facade(html){
-    return new Facade(
-            ""+html === html? parse(html) : html
-        )
-    }
 
 module.exports = facade;
