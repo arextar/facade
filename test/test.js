388e6e10b69bfb34dcb54b0a811703cdf80f1be5
arexkun <arexkun11@gmail.com>
2011-11-12 16:50:04 -0500

    add more testing for parsing

--- test/test.js
+++ test/test.js
@@ -26,6 +26,10 @@ var fragments = {
     "<b>asdf</b>": "<b>asdf</b>",
     
     
+    //Unary element
+    "<a />":"<a />",
+    
+    
     //Element left open
     "<b>asdf": "<b>asdf</b>",
     
@@ -50,6 +54,8 @@ var fragments = {
     '<div foo="bar" />':'<div foo=bar />',
     "<div foo='bar' />":'<div foo=bar />',
     '<div foo=bar />':'<div foo=bar />',
+    '<div foo=a@b.c />':'<div foo=a@b.c />',
+    '<textarea disabled />':'<textarea disabled  />',
     
     
     //Self-closing
@@ -59,9 +65,6 @@ var fragments = {
     //Closed by other
     '<dl><dt><dfn>happiness</dfn><dd>State of being happy<dt><dfn>sadness</dfn><dd>State of being sad</dl>':'<dl><dt><dfn>happiness</dfn></dt><dd>State of being happy</dd><dt><dfn>sadness</dfn></dt><dd>State of being sad</dd></dl>',
     
-    
-    //<p> closed by block element
-    '<p>Hi<div>Hi':'<p>Hi</p><div>Hi</div>',
     
     
     //doctypes are parsed out
@@ -89,7 +92,41 @@ var fragments = {
     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\'><img />':"<img />",
     '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Strict//EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\'><img />':"<img />"
     
-},x;
+},x, elem, block_elements = [ 'address', 'article', 'aside', 'blockquote', 'dir', 'div', 'dl', 'fieldset', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hgroup', 'hr', 'menu', 'nav', 'ol', 'p', 'pre', 'section', 'table', 'ul'],
+closes = [
+    'li', 'li',
+    'dt', 'dt',
+    'dt', 'dd',
+    'dd', 'dt',
+    'dd', 'dd',
+    'rp', 'rp',
+    'rp', 'rt',
+    'rt', 'rp',
+    'rt', 'rt',
+    'optgroup', 'optgroup',
+    'thead','tbody',
+    'thead','tfoot',
+    'tbody', 'tbody',
+    'tbody', 'tfoot',
+    'tfoot', 'tbody',
+    'tr', 'tr',
+    'td', 'td',
+    'td', 'th',
+    'th', 'td',
+    'th', 'th'
+]
+
+
+//<p> closed by block
+for(x=0; elem = block_elements[x++];){
+    fragments["<p>Text<"+elem+">Other text"] = "<p>Text</p>"+(elem=='hr'? "<hr />Other text" : "<"+elem+">Other text"+"</"+elem+">");
+    fragments["<p><b>elem<"+elem+"><i>other</i>"] = "<p><b>elem</b></p>"+(elem=='hr'? "<hr /><i>other</i>" : "<"+elem+"><i>other</i>"+"</"+elem+">");
+}
+
+for(x=0; elem = closes[x+=2];){
+    fragments["<"+closes[x]+">Text<"+closes[x+1]+">Other text"] = "<"+closes[x]+">Text</"+closes[x]+"><"+closes[x+1]+">Other text</"+closes[x+1]+">";
+    fragments["<"+closes[x]+"><b>elem<"+closes[x+1]+"><i>other</i>"] = "<"+closes[x]+"><b>elem</b></"+closes[x]+"><"+closes[x+1]+"><i>other</i></"+closes[x+1]+">";
+}
 
 
 for(x in fragments){
@@ -113,6 +150,7 @@ var document = facade("<div>\
 </span>\
 </div>\
 </div>\
+<i foo>Foo</i>\
 <var>a</var>")
 
 
@@ -129,6 +167,7 @@ var selectors = {
     "[foo^=ba]":"<em foo=bar>Hi</em><strong foo=baz>Hi</strong>",
     "[foo$=z]": "<strong foo=baz>Hi</strong><var foo=fez>Fezzes are cool</var>",
     "[foo*=a]": "<em foo=bar>Hi</em><strong foo=baz>Hi</strong>",
+    "[foo]": "<em foo=bar>Hi</em><strong foo=baz>Hi</strong><var foo=fez>Fezzes are cool</var><i foo>Foo</i>",
     "strong, var":"<strong>Hola</strong><strong foo=baz>Hi</strong><var foo=fez>Fezzes are cool</var><var>Hi</var><var>a</var>",
     "div var":"<var foo=fez>Fezzes are cool</var><var>Hi</var>",
     "div>var":"<var foo=fez>Fezzes are cool</var>",
