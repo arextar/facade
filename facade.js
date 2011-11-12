3b90440ec02c7bf3e31fc3f4997b4b1c43b87201
arexkun <arexkun11@gmail.com>
2011-11-12 13:14:21 -0500

    bring all manipulation logic out into functions that are accessable from the facade object (avoids problems in the future that could have been caused by a missing "parent" property

--- facade.js
+++ facade.js
@@ -12,7 +12,8 @@ function render( wi, what, parent ){
         wi(what)
     :
         wi
-    )
+    );
+    
     if(parent){
         var x = 0, e;
         for(;e=wi[x++];e.parent=parent);
@@ -28,7 +29,50 @@ function facade(html){
 
 var replace = facade.replace = function( what, wi ){
     var parent = what.parent;
-    [].splice.apply(v.parent, [v.parent.indexOf(what), 1].concat( render(wi, what, parent) ));
+    [].splice.apply(parent, [parent.indexOf(what), 1].concat( render(wi, what, parent) ));
+}
+
+var remove = facade.remove = function(what){
+    what.parent.splice(what.parent.indexOf(what), 1);
+}
+
+var before = facade.before = function(what, wi){
+    var parent = what.parent;
+    [].splice.apply( parent, [parent.indexOf(what), 0].concat( render( wi, what, parent ) ) );
+}
+
+var after = facade.after = function(what, wi){
+    var parent = what.parent;
+    [].splice.apply( parent, [parent.indexOf(what) + 1, 0].concat( render( wi, what, parent ) ) );
+}
+
+var append = facade.append = function(what, to){
+    [].push.apply(to.children, render( what, to, to) );
+}
+
+var prepend = facade.append = function(what, to){
+    [].unshift.apply(to.children, render( what, to, to) );
+}
+
+var attr = facade.attr = function(what, name, value){
+    
+    if(typeof name == "object"){
+        for(var x in name) attr(what, x, name[x]);
+    }
+    
+    var a=what.attr;
+    
+    if(value===false){
+        delete a[name];
+    }
+    else if(value===true){
+        a[name]=undefined;
+    }
+    else
+    {
+        a[name] = value;
+    }
+    
 }
 
 Facade.prototype = {
@@ -48,89 +92,51 @@ Facade.prototype = {
         return this;
     },
     replace: function(sel, w){
+        var rep = find( this.doc, sel ), x = 0, v;
         
-        var rep = find( this.doc, sel ), x = 0, v, fn = typeof w == "function";
-        
-        for(; v = rep[x++];){
-            replace(v, w);
-        }
+        for(;v = rep[x++];) replace(v, w);
         
         return this;
     },
     remove: function(sel){
-        
         var rep = find( this.doc, sel ), x = 0, v;
         
-        for(; v = rep[x++];){
-            v.parent.splice(
-                v.parent.indexOf(v),
-                1
-            );
-        }
+        for(; v = rep[x++];) remove(v);
         
         return this;
     },
     attr: function( sel, name, val ){
-        var rep = find( this.doc, sel ), x = 0, v;
+        var rep = find( this.doc, sel, true ), x = 0, v;
         
-        for(; v = rep[x++];){
-            if(val === false){
-                delete v.attr[name];
-            }
-            else if(val === true){
-                v.attr[name] = undefined;
-            }
-            else
-            {
-                v.attr[name] = val;
-            }
-        }
+        for(; v = rep[x++];) attr(v, name, val);
         
         return this;
     },
     before: function(sel, w){
+        var rep = find( this.doc, sel, true ), x = 0, v;
         
-        var rep = find( this.doc, sel, true ), x = 0, v, fn = typeof w == "function";
-        
-        for(; v = rep[x++];){
-            [].splice.apply(v.parent,
-                [v.parent.indexOf(v),
-                0].concat( parse( fn? w(v) : w ) )
-            );
-        }
+        for(; v = rep[x++];) before(v, w);
         
         return this;
     },
     after: function(sel, w){
+        var rep = find( this.doc, sel, true ), x = 0, v;
         
-        var rep = find( this.doc, sel, true ), x = 0, v, fn = typeof w == "function";
-        
-        for(; v = rep[x++];){
-            [].splice.apply(v.parent,
-                [v.parent.indexOf(v)+1,
-                0].concat( parse( fn? w(v) : w ) )
-            );
-        }
+        for(; v = rep[x++];) after(v, w);
         
         return this;
     },
     append: function(sel, w){
+        var rep = find( this.doc, sel, true ), x = 0, v;
         
-        var rep = find( this.doc, sel, true ), x = 0, v, fn = typeof w == "function";
-        
-        for(; v = rep[x++];){
-            [].push.apply(v.children, parse(w));
-        }
+        for(; v = rep[x++];) append(w, v);
         
         return this;
     },
     prepend: function(sel, w){
+        var rep = find( this.doc, sel, true ), x = 0, v;
         
-        var rep = find( this.doc, sel, true ), x = 0, v, fn = typeof w == "function";
-        
-        for(; v = rep[x++];){
-            [].unshift.apply(v.children, parse(w));
-        }
+        for(; v = rep[x++];) prepend(w, v);
         
         return this;
     }
